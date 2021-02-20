09f3941ff7f87a4c53009efcfe75ea2c52b617de
Dan Fielden <dan@danfielden.com>
2021-02-20 17:41:27 +0000

    Make GlorianaApplicationTests work

--- libraryapp/src/test/java/com/danfielden/gloriana/GlorianaApplicationTests.java
+++ libraryapp/src/test/java/com/danfielden/gloriana/GlorianaApplicationTests.java
@@ -1,13 +1,18 @@
 package com.danfielden.gloriana;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"goat=/tmp/gloriana_test.db"})
 class GlorianaApplicationTests {
+
+    @Value("goat")
+    String databasePath;
 
     @Test
     void contextLoads() {
+        // This test is empty, and just makes sure the application can boot with the default settings.
     }
 
 }
